9b2fb893e0ea882e7d2107669945649cfe077653
Lopezender <95137344+Lopezender@users.noreply.github.com>
2023-01-07 08:17:07 -0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyClVT8RRbqCgQTWRUSAwQBWrk-x_8WFXEY",
+    authDomain: "crud-notas-c14ec.firebaseapp.com",
+    projectId: "crud-notas-c14ec",
+    storageBucket: "crud-notas-c14ec.appspot.com",
+    messagingSenderId: "26501572202",
+    appId: "1:26501572202:web:bed75f15f9f7785131cc13"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
